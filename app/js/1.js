7859b4cf4b9feb59773d468f65b3e8c594d2f85a
sebolith <reginald.w.mathieu@gmail.com>
2014-06-12 19:48:06 -0400

    pulled reggie's changes into master

---
+++ app/js/1.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("SportifStore",["ngResource","ngStorage","ngSanitize","ui.router","restangular","controllers","directives","services","factories","filters"]),angular.module("controllers",[]),angular.module("directives",[]),angular.module("services",[]),angular.module("factories",[]),angular.module("filters",[]);
