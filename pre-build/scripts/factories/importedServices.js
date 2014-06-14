036846620bea8a19d95c004a82ceef6d0abd421a
sebolith <reginald.w.mathieu@gmail.com>
2014-06-14 11:44:06 -0400

    refactoring home slider

--- pre-build/scripts/factories/importedServices.js
+++ pre-build/scripts/factories/importedServices.js
@@ -1,57 +1,27 @@
 /* Factories */
 angular.module('factories')
 
-.factory( 'QuickShopService' , ["$http", function ($http) {
-    var model = {
-        contents: null
-    };
-    return {
-        model: model,
+    .factory('SliderPanelService', [function(){
+        var panel = {
+            left: 0,
+            top: 0,
+            width: 0,
 
-        getProductData: function ( code, callback ) {
-            var promise = $http.get( globals.contextPath + '/json/product/getLightweightProductData.json?productCode=' + code ).success( function ( data ) {
-                if( data.primaryImage ) {
-                    data.primaryImage.url = data.primaryImage.url.indexOf('?') === -1 ? data.primaryImage.url +'?time='+ ( new Date().getTime() ) :
-                        data.primaryImage.url +'&time='+ ( new Date().getTime() );
-                }
-                model.contents = data;
-                // callback();
-            }).error( function ( err ) {
-                if(window.console)
-                    window.console.log(err);
-            });
+            setLeft: function(value){
+                panel.left = value;
+            },
 
-            return promise;
-        }
-    };
-}])
-.factory('QuickShopPanelService', ["QuickShopService", "$rootScope", function(QuickShopService, $rootScope){
-    var panel = {
-        left: 0,
-        top: 0,
-        width: 0,
+            setTop: function(value){
+                panel.top = value;
+            },
 
-        setLeft: function(value){
-            panel.left = value;
-        },
+            setWidth: function(value){
+                panel.width = value;
+            }
+        };
 
-        setTop: function(value){
-            panel.top = value;
-        },
-
-        setWidth: function(value){
-            panel.width = value;
-        },
-
-        getProductData: function(productCode){
-            $rootScope.$broadcast('quickShopLoading');
-            return QuickShopService.getProductData( productCode );
-        }
-
-    };
-
-    return {
-        panel: panel
-    };
-}]);
+        return {
+            panel: panel
+        };
+    }]);
 
