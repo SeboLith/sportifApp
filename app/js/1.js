ebcac8ffed0b72e3a4ec8c902de0983692082e11
sebolith <reginald.w.mathieu@gmail.com>
2014-06-21 18:03:37 -0400

    fixed merge

--- app/js/1.js
+++
@@ -1,1 +0,0 @@
-"use strict";angular.module("SportifStore",["ngResource","ngStorage","ngSanitize","ui.router","restangular","controllers","directives","services","factories","filters"]),angular.module("controllers",[]),angular.module("directives",[]),angular.module("services",[]),angular.module("factories",[]),angular.module("filters",[]);
